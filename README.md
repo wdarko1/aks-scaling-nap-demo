d9e44861626515f64d085c91dd87f6d5875d1a42
Ahmed Sabbour <asabbour@microsoft.com>
2022-10-20 13:01:02 -0700

    Updated to use Prometheus scaler for KEDA

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## Setup
 1. Login using `az login`
 1. Make sure `kubectl` is installed
+1. Make sure `yq` is installed (https://github.com/mikefarah/yq/#install)
 1. Make sure `hey` is installed (https://github.com/rakyll/hey)
 1. Run `./run.sh`
 
--- run.sh
+++ run.sh
@@ -14,6 +14,13 @@ read CLUSTER_RG
 CLUSTER_RG="${CLUSTER_RG:=${PREFIX}${RANDOMSTRING}-rg}"
 echo $CLUSTER_RG
 
+echo -e "aksexperiences.azurecr.io admin username (default: aksexperiences): \c"
+read ACRUSER
+LOCATION="${ACRUSER:=aksexperiences}"
+
+echo -e "aksexperiences.azurecr.io admin password: \c"
+read ACRPASSWORD
+
 CLUSTER_NAME=${PREFIX}${RANDOMSTRING}
 VNET_NAME=${PREFIX}${RANDOMSTRING}
 VNET_RG=${CLUSTER_RG}
@@ -68,32 +75,38 @@ az aks create -n ${CLUSTER_NAME} -g ${CLUSTER_RG} \
 --node-vm-size Standard_DS2_v2 \
 --kubernetes-version 1.24.6
 
-#echo "Enabling Prometheus metric collection using Azure Monitor"
-#az aks update --enable-azuremonitormetrics -n ${CLUSTER_NAME} -g ${CLUSTER_RG}
+# Retrieve the Log Analytics workspace and Azure monitor workspace details
+echo "Retrieving the AKS tenant id"
+TENANT_ID=$(az aks show -n ${CLUSTER_NAME} -g ${CLUSTER_RG} --query identity.tenantId -o tsv)
+
+# Enable Prometheus metric collection using Azure Monitor
+echo "Enabling Prometheus metric collection using Azure Monitor"
+az aks update --enable-azuremonitormetrics -n ${CLUSTER_NAME} -g ${CLUSTER_RG}
+
+# Retrieve the Log Analytics workspace and Azure monitor workspace details
+echo "Retrieving the Log Analytics workspace resource id"
+LOGANALYTICSWORKSPACE_RESOURCE_ID=$(az aks show -n ${CLUSTER_NAME} -g ${CLUSTER_RG} --query addonProfiles.omsagent.config.logAnalyticsWorkspaceResourceID -o tsv)
+
+echo "Retrieving the Log Analytics workspace resource group"
+LOGANALYTICSWORKSPACE_RG=$(az resource show --id $LOGANALYTICSWORKSPACE_RESOURCE_ID --query "resourceGroup" -o tsv)
+
+echo "Retrieving the Azure Monitor workspace resource id"
+AZUREMONITORWORKSPACE_RESOURCE_ID=$(az resource list -g ${LOGANALYTICSWORKSPACE_RG} --resource-type microsoft.monitor/accounts --query "[?starts_with(name,'defaultazuremonitorworkspace')].id" -o tsv)
+
+echo "Retrieving the Azure Monitor workspace Prometheus query endpoint"
+AZUREMONITOR_PROM_ENDPOINT=$(az resource show --id $AZUREMONITORWORKSPACE_RESOURCE_ID --query "properties.metrics.prometheusQueryEndpoint" -o tsv | sed -e "s/https:\/\///" )
+
+# Retrieve the kubelet identity
+echo "Retrieving kubelet identity clientId for cluster ${CLUSTER_NAME}"
+KUBELETIDENTITY_CLIENT_ID=$(az aks show -g ${CLUSTER_RG} -n ${CLUSTER_NAME} --query "identityProfile.kubeletidentity.objectId" -o tsv)
+
+# Assigning the Monitoring Data Reader role
+echo "Assigning the Monitoring Data Reader role"
+az role assignment create --assignee-object-id ${KUBELETIDENTITY_CLIENT_ID} --assignee-principal-type ServicePrincipal --role b0d8363b-8ddd-447d-831f-62ca05bff136 --scope ${AZUREMONITORWORKSPACE_RESOURCE_ID}
 
 # Retrieve AKS cluster credentials
 echo "Retrieving the Azure Kubernetes Service cluster credentials"
 az aks get-credentials -n ${CLUSTER_NAME} -g ${CLUSTER_RG}
-
-# Add the nginx Helm repo
-echo "Adding the nginx Helm repo"
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm repo update
-
-# Install nginx
-echo "Installing nginx into kube-system namespace"
-helm install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace kube-system
-sleep 10
-
-# Add the Keda Core Helm repo
-echo "Adding the kedacore Helm repo"
-helm repo add kedacore https://kedacore.github.io/charts
-helm repo update
-
-# Install the KEDA HTTP Add-on
-echo "Installing the KEDA HTTP add-on into kube-system namespace along with the KEDA add-on"
-helm install http-add-on kedacore/keda-add-ons-http --namespace kube-system
-sleep 10
 
 END="$(date +%s)"
 DURATION=$[ ${END} - ${START} ]
@@ -107,10 +120,30 @@ echo "Total time elapsed: $(( DURATION / 60 )) minutes"
 echo ""
 
 echo "========================================================"
-echo "|                DEPLOYING THE APPLICATION             |"
+echo "|               ADD THE IMAGE PULL SECRET              |"
 echo "========================================================"
 echo ""
 kubectl create ns serverloader
+kubectl create secret docker-registry aksexperiences-secret \
+    --namespace kube-system \
+    --docker-server=aksexperiences.azurecr.io \
+    --docker-username=${ACRUSER} \
+    --docker-password=${ACRPASSWORD}
+
+echo "========================================================"
+echo "|           UPDATING PROMETHEUS AUTH CONFIG            |"
+echo "========================================================"
+echo ""
+yq -i "(.spec.template.spec.containers[0].env[] | select(.name == \"AAD_CLIENT_ID\").value) = \"${KUBELETIDENTITY_CLIENT_ID}\"" manifests/vpa-keda/keda-prom-auth-deployment.yaml
+yq -i "(.spec.template.spec.containers[0].env[] | select(.name == \"AAD_TENANT_ID\").value) = \"${TENANT_ID}\"" manifests/vpa-keda/keda-prom-auth-deployment.yaml
+yq -i "(.spec.template.spec.containers[0].env[] | select(.name == \"TARGET_HOST\").value) = \"${AZUREMONITOR_PROM_ENDPOINT}\"" manifests/vpa-keda/keda-prom-auth-deployment.yaml
+
+
+echo "========================================================"
+echo "|               DEPLOYING THE APPLICATION              |"
+echo "|                 AND PROMETHEUS CONFIG                |"
+echo "========================================================"
+echo ""
 kubectl apply -f ./manifests/vpa-keda
 
 echo ""
@@ -132,6 +165,7 @@ echo "========================================================"
 echo ""
 echo "=============================="
 echo "curl -k http://${SERVICE_IP}/workout"
+echo "curl -k http://${SERVICE_IP}/metrics"
 echo "curl -k http://${SERVICE_IP}/stats"
 echo ""
 echo ""
@@ -139,7 +173,7 @@ echo "========================================================"
 echo "|                   GENERATE LOAD                      |"
 echo "========================================================"
 echo ""
-echo "hey -n 200000 -c 500 http://${SERVICE_IP}/workout"
+echo "hey -n 200000 -c 600 http://${SERVICE_IP}/workout"
 
 echo ""
 echo "========================================================"
